5c3ce17f21df920875954eb1eb6314c91b3f6fff
Nick Elliott <nelliott@liquidus.net>
2018-11-29 17:54:28 -0600

    progress

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const port = 2047;
-
-const watch = require('./watch.js');
-
 const app = express();
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
 
 app.enable('view cache');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+fs.watch('./index.js')
 
 // ROUTES
 
 app.get('/', (req, res, next) => {
-  res.render('pages/home');
+
+  let js = 'x = 13;';
+
+  let md5sum = crypto.createHash('md5').update(js).digest('hex');
+
+  let jspath = path.join(__dirname, '../public/jstemplates', md5sum + '.js');
+
+
+  fs.stat(jspath, (err, stats) => {
+    if (err) {
+      console.log('writing file: ' + jspath);
+      fs.writeFile(jspath, js, (err) => {
+          if (err) throw err;
+          res.set({"Content-Disposition":"attachment; filename=\"" + md5sum + "\""});
+          res.send(js);
+      });
+    }
+    else {
+      console.log('sending existing file: ' + jspath);
+      res.set({"Content-Disposition":"attachment; filename=\"" + md5sum + "\""});
+      res.sendFile(jspath);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
