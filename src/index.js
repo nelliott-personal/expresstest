dc563beb12408b365e8d9bcec0fd930c0183bf85
Nick Elliott <nelliott@liquidus.net>
2018-11-29 21:29:41 -0600

    create dir

--- src/index.js
+++ src/index.js
@@ -13,17 +13,19 @@ const _ = require('lodash'),
 const app = express(),
       port = process.env.PORT;
 
+
+
 // ROUTES
 
 app.get('/js/:hash?', (req, res, next) => {
+  const dir = './jstemplates';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
   const js = uglify.minify(JSGenerator(), { mangle:{ toplevel: true } }).code;
   const filename = `${crypto.createHash('md5').update(js).digest('hex')}.js`;
-  const jspath = path.join(__dirname, '../public/jstemplates', `${req.params.hash || filename}`);
+  const jspath = `./jstemplates/${req.params.hash || filename}`;
 
-  var dir = __dirname + '/upload';
-  if (!path.existsSync('./public')) {
-      fs.mkdirSync('./public');
-  }
   res.set('Content-Type', 'text/javascript');
 
   fs.stat(jspath, (err, stats) => {
